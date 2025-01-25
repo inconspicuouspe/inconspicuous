d96f0d82110f29483950647b4957b64abb07bc10
TheCommCraft <simon.c.gilde@gmail.com>
2025-01-25 19:36:55 +0100

    added endpoints

--- api/index.py
+++ api/index.py
@@ -6,3 +6,11 @@ app = Flask(__name__, template_folder="templates")
 @app.get("/")
 def home():
     return render_template("login.html")
+
+@app.get("/login/")
+def login():
+    pass
+
+@app.get("/register/")
+def register():
+    pass
