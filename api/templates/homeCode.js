99cd908df4524f891d09350b95bb8376f58454af
TheCommCraft <simon.c.gilde@gmail.com>
2025-04-27 18:19:12 +0200

    settingsvalue

--- api/templates/homeCode.js
+++ api/templates/homeCode.js
@@ -11,11 +11,26 @@ logoutButton.addEventListener("click", (event) => {
 {% if Settings._CREATE_MEMBERS in session.settings %}
 const settingsValues = {
     {% for setting in Settings.__members__.values() %}
+    {% if setting in session.settings %}
     setting_{{ setting.name }}: {{ setting.value }},
+    {% endfor %}
     {% endfor %}
 };
 const addUserButton = document.querySelector("#addUserForm");
+const settingsDiv = document.querySelector("div.settings-box");
 addUserButton.addEventListener("submit", (event) => {
-    fetch("{{ url_for('add_user') }}");
+    const settingsValue = 0;
+    for (const child of settingsDiv.children) {
+        settingsValue |= settingsValues[child.children[0].name] * child.children[0].checked;
+    }
+    console.log(settingsValue);
+    /*fetch("{{ url_for('add_user') }}" {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify()
+    });*/
 })
 {% endif %}
