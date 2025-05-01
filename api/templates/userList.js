2de2fdeaa99028b4a3de23718dde786e86947c1e
TheCommCraft <simon.c.gilde@gmail.com>
2025-05-01 17:04:02 +0200

    option

--- api/templates/userList.js
+++ api/templates/userList.js
@@ -21,19 +21,27 @@ function userListSuccess(data){
         const settings = user.{{ consts.FIELD_SETTINGS }};
         const currentLine = createUserListLine();
         currentLine.querySelector(".username-column").textContent = username;
-        currentLine.querySelector(".pgroup-column").textContent = pgroup;
+        currentLine.querySelector(".pgroup-column").textContent = pgroup ? pgroup !== -1 : "?";
         const settingsList = currentLine.querySelector(".settings-column .settings-list")
-        for (const name in settingsValues) {
-            if (!name.startsWith("setting__") && name != "setting_NONE" && (settingsValues[name] & settings) === settingsValues[name]) {
-                console.log(name, username);
-                const settingElm = document.createElement("li");
-                settingElm.textContent = settingsNames[name];
-                settingsList.appendChild(settingElm);
+        if (settings === -1){
+            const settingElm = document.createElement("li");
+            settingElm.textContent = "?";
+            settingsList.appendChild(settingElm);
+        } else {
+            for (const name in settingsValues) {
+                if (!name.startsWith("setting__") && name !== "setting_NONE" && (settingsValues[name] & settings) === settingsValues[name]) {
+                    console.log(name, username);
+                    const settingElm = document.createElement("li");
+                    settingElm.textContent = settingsNames[name];
+                    settingsList.appendChild(settingElm);
+                }
             }
         }
-        const optionButton = createUserListButton();
-        optionButton.textContent = "Option";
-        currentLine.querySelector(".options-column").appendChild(optionButton);
+        if (pgroup < ownPgroup) {
+            const optionButton = createUserListButton();
+            optionButton.textContent = "Option";
+            currentLine.querySelector(".options-column").appendChild(optionButton);
+        }
         userListTable.appendChild(currentLine);
     }
     userListTable.style.display = "";
